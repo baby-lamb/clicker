22585b71b251269ad2efa99e149d955c03ae00b2
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-04-15 11:54:41 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="main">
-      <CounterMenu></CounterMenu>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter : 0
+    }
+  }
+  handleClick(event){
+    this.setState({counter: ++this.state.counter})
+  }
+  render(){
+    return (
+      <div className="App">
+        <button
+          onClick={this.handleClick.bind(this)}
+          className="btn">
+          click {this.state.counter} times
+          </button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
