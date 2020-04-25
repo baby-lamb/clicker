58c10352be8bd11a9e48c133b33db9affa26dec5
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-04-26 01:22:34 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -5,41 +5,28 @@ import cookie from './img/cookie.png';
 class App extends React.Component {
   constructor(props){
     super(props)
+    const click=[2, 5, 10];
     this.state = {
       counter : 0,
     }
   }
 
-  handleClick(event){
-    this.setState({counter: ++this.state.counter})
+  handler(evnet){
+
   }
 
+  handleClick(event){
+    this.setState({counter: ++this.state.counter})
+
+  }
   handleClickOne(event){
     this.setState({counter: +2+this.state.counter})
   }
 
-  handleClickTwo(event){
-    this.setState({counter: +5+this.state.counter})
-  }
 
-  handleClickThree(event){
-    this.setState({counter: +10+this.state.counter})
-  }
-
-  handleClickFour(event){
-    this.setState({counter: +20+this.state.counter})
-  }
-
-  handleClickFive(event){
-    this.setState({counter: +50+this.state.counter})
-  }
-
-  handleSecound(event){
-    this.setState({counter: setInterval(++this.counter,1000)})
-
-  }
 
   render(){
+
     return (
       <div className="App">
         <div
@@ -54,24 +41,27 @@ class App extends React.Component {
           </div>
           <ul>
             <li
-              onClick={this.handleClickOne.bind(this)}>
+              onClick={this.handleClick.bind(this)}
+              >
               +1
             </li>
             <li
-              onClick={this.handleClickTwo.bind(this)}>+5</li>
+              onClick={this.handleClick.bind(this)}
+              >+5</li>
             <li
-              onClick={this.handleClickThree.bind(this)}>+10</li>
+              >+10</li>
             <li
-              onClick={this.handleClickFour.bind(this)}
+
             >+20</li>
             <li
-              onClick={this.handleClickFive.bind(this)}
+
             >+50</li>
+
           </ul>
 
           <ul>
             <li
-              onClick={this.handleSecound.bind(this)}>1</li>
+              >1</li>
             <li>2</li>
             <li>3</li>
             <li>4</li>
