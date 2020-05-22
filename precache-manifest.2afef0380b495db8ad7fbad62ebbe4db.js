b4fcd7835b7f24a71dabd3c612d0b965bfe40f15
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-05-22 22:25:49 +0900

    Updates

---
+++ precache-manifest.2afef0380b495db8ad7fbad62ebbe4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcd54f74ab96795c248d1f7ea680420",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "87cbf3033f069b8117fa",
+    "url": "/clicker/static/css/main.bead91d7.chunk.css"
+  },
+  {
+    "revision": "58bc3eb2f51f0bbf9ff9",
+    "url": "/clicker/static/js/2.867f7dd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicker/static/js/2.867f7dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87cbf3033f069b8117fa",
+    "url": "/clicker/static/js/main.7a33f7e1.chunk.js"
+  },
+  {
+    "revision": "123346803d5a47db9fca",
+    "url": "/clicker/static/js/runtime-main.e29ea3f6.js"
+  },
+  {
+    "revision": "1550cf1ffaa3de662a5096527b679f76",
+    "url": "/clicker/static/media/cookie.1550cf1f.png"
+  }
+]);
