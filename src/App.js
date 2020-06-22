e2e9308de19fa653acd1008b79b46a75e80a3249
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-06-22 20:52:37 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
           </div>
             <ul className="shop">
               <li><img src={shop} /></li>
-              <li><img src={shop} /></li>
+              <li><img src={shop} className={this.state.isOn ? 'see' : 'none'} /></li>
               <li><img src={shop} className={this.state.isOn ? 'see' : 'none'} /></li>
               <li><img src={shop} className={this.state.isOn ? 'see' : 'none'} /></li>
               <li><img src={shop} className={this.state.isOn ? 'see' : 'none'} /></li>
